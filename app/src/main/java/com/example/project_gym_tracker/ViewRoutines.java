94e00069c01d697281345e3d2604c0b6397f5027
alberto.balderas <alberto.balderas@uabc.edu.mx>
2024-06-04 20:05:08 -0700

    ahi la llevo (estable)

---
+++ app/src/main/java/com/example/project_gym_tracker/ViewRoutines.java
@@ -0,0 +1,14 @@
+package com.example.project_gym_tracker;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ViewRoutines extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_reps_and_weight);
+    }
+}
