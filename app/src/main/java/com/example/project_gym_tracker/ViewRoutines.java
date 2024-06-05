a20bb695ff341f1b44ff076f6264c4079e87d960
alberto.balderas <alberto.balderas@uabc.edu.mx>
2024-06-04 20:06:19 -0700

    a

--- app/src/main/java/com/example/project_gym_tracker/ViewRoutines.java
+++ app/src/main/java/com/example/project_gym_tracker/ViewRoutines.java
@@ -1,14 +1,22 @@
 package com.example.project_gym_tracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class ViewRoutines extends AppCompatActivity {
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class ViewRoutines extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_reps_and_weight);
+        setContentView(R.layout.add_routines);
     }
+
 }
--- app/src/main/java/com/example/project_gym_tracker/WelcomeActivity.java
+++ app/src/main/java/com/example/project_gym_tracker/WelcomeActivity.java
@@ -1,7 +1,11 @@
 package com.example.project_gym_tracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
@@ -12,6 +16,7 @@ public class WelcomeActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RoutinesAdapter adapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +24,19 @@ public class WelcomeActivity extends AppCompatActivity {
 
         // Crear una lista de rutinas con ejercicios de ejemplo (puedes reemplazar esto con tus propios datos)
         List<Routine> routines = new ArrayList<>();
+
         List<Exercise> exercises1 = new ArrayList<>();
-        exercises1.add(new Exercise("Ejercicio 1", "Piernas", 10, 3, 50.0));
-        exercises1.add(new Exercise("Ejercicio 2", "Espalda", 12, 4, 60.0));
-        Routine routine1 = new Routine("Rutina 1", exercises1);
+        List<Exercise> exercises2 = new ArrayList<>();
+
+        exercises1.add(new Exercise("Ejercicio prueba 1", "Piernas", 10, 3, 50.0));
+        exercises1.add(new Exercise("Ejercicio prueba 2", "Espalda", 12, 4, 60.0));
+        Routine routine1 = new Routine("Rutina Prueba 1", exercises1);
         routines.add(routine1);
 
-        List<Exercise> exercises2 = new ArrayList<>();
-        exercises2.add(new Exercise("Ejercicio 3", "Pecho", 8, 3, 70.0));
-        exercises2.add(new Exercise("Ejercicio 4", "Brazos", 15, 3, 40.0));
-        Routine routine2 = new Routine("Rutina 2", exercises2);
+
+        exercises2.add(new Exercise("Ejercicio prueba 3", "Pecho", 8, 3, 70.0));
+        exercises2.add(new Exercise("Ejercicio prueba 4", "Brazos", 15, 3, 40.0));
+        Routine routine2 = new Routine("Rutina Prueba 2", exercises2);
         routines.add(routine2);
 
         // Configurar el RecyclerView
@@ -36,5 +44,22 @@ public class WelcomeActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new RoutinesAdapter(routines);
         recyclerView.setAdapter(adapter);
+
+
+        Button addRoutineButton = findViewById(R.id.addRoutineButton);
+        addRoutineButton.setOnClickListener(view -> {
+            Intent intent = new Intent(WelcomeActivity.this, ViewRoutines.class);
+            startActivity(intent);
+
+        });
     }
 }
+
+
+
+
+
+
+
+
+
