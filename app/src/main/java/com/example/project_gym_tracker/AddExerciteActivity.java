f79b335755fe952c3dbfa31b83a2c87630cf44bc
alberto.balderas <alberto.balderas@uabc.edu.mx>
2024-06-05 20:19:11 -0700

    ahi la llevamos compadre (solida como pata de perro envenenada)

--- app/src/main/java/com/example/project_gym_tracker/AddExerciteActivity.java
+++ app/src/main/java/com/example/project_gym_tracker/AddExerciteActivity.java
@@ -1,6 +1,8 @@
 package com.example.project_gym_tracker;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,12 @@ public class AddExerciteActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.add_exercite);
 
+        Spinner categoryexercitespinner = findViewById(R.id.categoryexercitespinner);
+        String[] datos = new String[] {"","Abdominales", "Espalda", "Biceps", "Pecho", "Pierna","Hombros","Triceps"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, datos);
+
+        categoryexercitespinner.setAdapter(adapter);
 
 
     }
--- app/src/main/java/com/example/project_gym_tracker/addWeightandRepsActivity.java
+++ app/src/main/java/com/example/project_gym_tracker/addWeightandRepsActivity.java
@@ -1,6 +1,9 @@
 package com.example.project_gym_tracker;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,8 +19,59 @@ public class addWeightandRepsActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.add_weight_and_reps);
 
+        Button restarPesoBtn = findViewById(R.id.restarPesobtn);
+        Button sumarPesoBtn = findViewById(R.id.sumarPesobtn);
+        Button restarRepBtn = findViewById(R.id.restarrepbtn);
+        Button sumarRepBtn = findViewById(R.id.sumarrepbtn);
+        EditText pesoEditText = findViewById(R.id.editTextNumberDecimal);
+        EditText repeticionesEditText = findViewById(R.id.editTextNumberDecimal2);
 
+        // Sumar y Restar Peso
+        restarPesoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                modificarValor(pesoEditText, -1);
+            }
+        });
 
+        sumarPesoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                modificarValor(pesoEditText, 1);
+            }
+        });
 
+        // Sumar y Restar Repeticiones
+        restarRepBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                modificarValor(repeticionesEditText, -1);
+            }
+        });
+
+        sumarRepBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                modificarValor(repeticionesEditText, 1);
+            }
+        });
     }
+
+    private void modificarValor(EditText editText, int cambio) {
+        String valorStr = editText.getText().toString();
+        int valor;
+        if (valorStr.isEmpty()) {
+            valor = 0;
+        } else {
+            valor = Integer.parseInt(valorStr);
+        }
+        valor += cambio;
+        if (valor < 0) {
+            valor = 0; // Asegurarse de que el valor no sea negativo
+        }
+        editText.setText(String.valueOf(valor));
+    }
+
+
+
 }
